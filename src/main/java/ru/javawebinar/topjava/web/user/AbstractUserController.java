a89930c58bb432c1a91ff753b8901eb708d50859
Daniil <53508353+danyaMiller@users.noreply.github.com>
2020-11-12 13:04:05 +0300

    6_4

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -14,6 +14,7 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
+
     @Autowired
     private UserService service;
 
