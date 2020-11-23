b5c1e074cf69b9b01dc20d2f05d8029b1dc657f1
Daniil <53508353+danyaMiller@users.noreply.github.com>
2020-11-23 18:48:30 +0300

    HW07Start1

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealRestControllerTest {
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void updateOrCreate() {
+    }
+
+    @Test
+    void getBetween() {
+    }
+}
