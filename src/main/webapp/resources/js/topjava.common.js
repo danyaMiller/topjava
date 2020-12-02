af0370c8c13adb67864a2c0b39d028ed225ab6eb
Daniil <53508353+danyaMiller@users.noreply.github.com>
2020-12-02 16:34:25 +0300

    8_8

---
+++ src/main/webapp/resources/js/topjava.common.js
@@ -0,0 +1,41 @@
+var form;
+
+function makeEditable() {
+    form = $('#detailsForm');
+    $(".delete").click(function () {
+        if (confirm('Are you sure?')) {
+            deleteRow($(this).attr("id"));
+        }
+    });
+}
+
+function add() {
+    form.find(":input").val("");
+    $("#editRow").modal();
+}
+
+function deleteRow(id) {
+    $.ajax({
+        url: ctx.ajaxUrl + id,
+        type: "DELETE"
+    }).done(function () {
+        updateTable();
+    });
+}
+
+function updateTable() {
+    $.get(ctx.ajaxUrl, function (data) {
+        ctx.datatableApi.clear().rows.add(data).draw();
+    });
+}
+
+function save() {
+    $.ajax({
+        type: "POST",
+        url: ctx.ajaxUrl,
+        data: form.serialize()
+    }).done(function () {
+        $("#editRow").modal("hide");
+        updateTable();
+    });
+}
